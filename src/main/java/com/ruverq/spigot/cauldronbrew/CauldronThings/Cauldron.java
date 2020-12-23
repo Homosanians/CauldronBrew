328a9433b47f4c70a4342d650433c57f5e65c50b
Alexander Emelyanov <contact.emelyanov@gmail.com>
2020-12-23 17:17:41 +0300

    Update Cauldron.java
    Fixes RP #1

--- src/main/java/com/ruverq/spigot/cauldronbrew/CauldronThings/Cauldron.java
+++ src/main/java/com/ruverq/spigot/cauldronbrew/CauldronThings/Cauldron.java
@@ -3,7 +3,6 @@ package com.ruverq.spigot.cauldronbrew.CauldronThings;
 import com.ruverq.spigot.cauldronbrew.CauldronThings.ParticleManager.ParticleManager;
 import com.ruverq.spigot.cauldronbrew.CauldronThings.Hologramchiki.Hologram;
 import com.ruverq.spigot.cauldronbrew.Main;
-import com.sun.istack.internal.Nullable;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -487,7 +486,6 @@ public class Cauldron {
         }.runTaskTimer(Main.getInstance().getInstance(), 0, period / 10);
     }
 
-    @Nullable
     public static Cauldron getCauldron(Block block){
         return blockCauldronHashMap.get(block);
     }
